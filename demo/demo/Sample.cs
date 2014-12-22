0202067c3a6d1abcaac5c16f2afc1d524c771f34
Janusz Tomaszewski <tomaszewski.janusz@gmail.com>
2014-12-22 22:43:50 +0100

    First commit

---
+++ demo/demo/Sample.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace demo
+{
+    class Sample
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public bool IsTestingFun()
+        {
+            return true;
+        }
+    }
+
+    [TestFixture]
+    public class SampleTest
+    {
+        [TestCase(1, 1)]
+        [TestCase(-1, -1)]
+        [TestCase(-1, 1)]
+        [TestCase(1, -1)]
+        public void AddReturnsSumOfBothParams(int a, int b)
+        {
+            Sample s = new Sample();
+            int result = s.Add(a, b);
+
+            int expected = a + b;
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void IsTestingFunReturnsTure()
+        {
+            Sample s = new Sample();
+            bool result = s.IsTestingFun();
+
+            Assert.AreEqual(true, result);
+        }
+    }
+}
