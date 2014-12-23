088b85750eb2401ebc2f500b857cfe3aae900403
Janusz Tomaszewski <tomaszewski.janusz@gmail.com>
2014-12-23 13:57:18 +0100

    First unit test

--- BasicUnitTests/BasicUnitTests/Sample.cs
+++ BasicUnitTests/BasicUnitTests/Sample.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
 
 namespace BasicUnitTests
 {
@@ -15,6 +16,20 @@ namespace BasicUnitTests
         public bool IsTestingFun()
         {
             return true;
+        }
+    }
+
+    [TestFixture]
+    public class SampleTest
+    {
+        [Test]
+        public void AddReturnsSumOfBothParams()
+        {
+            Sample s = new Sample();
+
+            int result = s.Add(1, 1);
+
+            Assert.AreEqual(2, result);
         }
     }
 }
