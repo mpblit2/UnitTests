4191b7df958c70a27d87ab97fdafd94d0496323e
Janusz Tomaszewski <tomaszewski.janusz@gmail.com>
2014-12-23 12:48:25 +0100

    Usunieto pliki

---
+++ BasicUnitTests/BasicUnitTests/Sample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BasicUnitTests
+{
+    class Sample
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public bool IsTestingFun()
+        {
+            return true;
+        }
+    }
+}
